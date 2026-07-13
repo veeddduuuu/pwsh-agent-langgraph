585ddc6626d3deacc5f1b120db7a4c41ee1783ba
Vedant Shende <btech10662.24@bitmesra.ac.in>
2026-07-13 09:48:59 +0530

    Add  implementation of workflow graph for command execution

---
+++ agent/graph.py
@@ -0,0 +1,48 @@
+from langgraph.graph import END, StateGraph
+from .nodes import generate_command_node, execute_command_node, blocked_node, route_after_safety, screen_command_node
+from .state import AgentState
+from langgraph.checkpoint.memory import END, MemorySaver
+import uuid
+
+def build_graph():
+    workflow = StateGraph(AgentState) 
+
+    workflow.add_node("generate_command", generate_command_node)
+    workflow.add_node("screen_command", screen_command_node)
+    workflow.add_node("execute_command", execute_command_node)
+    workflow.add_node("blocked", blocked_node)
+
+    workflow.set_entry_point("generate_command")
+
+    workflow.add_edge("generate_command", "screen_command")
+    workflow.add_edge("screen_command", "execute_command")
+    workflow.add_conditional_edges(
+        "screen_command",
+        route_after_safety,
+        {
+            "execute":"execute_command",
+            "block":"exit_codeblocked",
+        }
+    )
+    workflow.add_edge("execute_command", END)
+    workflow.add_edge("blocked", END)
+
+    return workflow.compile(
+        checkpointer=MemorySaver(),
+        interupt_before=["execute_command"],
+    )
+
+def thread_config():
+    return {"configurable" : {"thread_id" : str(uuid.uuid4())}}
+
+def run_agent(prompt: str, auto: bool = True) -> AgentState:
+    graph = build_graph()
+    config = thread_config()
+
+    graph.invoke({"prompt":prompt}, config)
+
+    if auto :
+        graph.invoke(None, config)
+
+    return graph.get_state(config).values
+    
